9d25b7948592882904b505a1fd650349b381079d
kramoth <kramoth@udm.ac.mu>
2020-06-29 10:56:53 +0400

    [dev] fix fifo value

--- namedPipe/reader.c
+++ namedPipe/reader.c
@@ -10,6 +10,7 @@ int main()
 	char pipeName[]="pipe.fifo";
 	char messageToRead[MESSAGE_SIZE];
 	int descriptor0=open(pipeName,O_RDONLY);
+	int ret;
 	if(descriptor0==-1)
 	{
 		printf("failed to open named pipe\n");
@@ -17,4 +18,9 @@ int main()
 	}
 	read(descriptor0,messageToRead,MESSAGE_SIZE);
 	printf("received %s\n", messageToRead);
+	ret=unlink("pipe.fifo");
+	if(ret==0)
+	{
+		printf("fifo file deleted\n");
+	}
 }
